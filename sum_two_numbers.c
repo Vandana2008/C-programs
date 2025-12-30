d6e8f27715be73c8381cf3c45ad79d932ba25368
Vandana2008 <vandanakethavarapu39@gmail.com>
2025-12-30 21:55:31 +0530

    sum of two numbers in c

---
+++ sum_two_numbers.c
@@ -0,0 +1,11 @@
+//program to print sum of two numbers
+#include<stdio.h>
+int main()
+{
+  int a,b,sum;
+printf("enter two numbers\n");
+scanf("%d%d",&a,&b);
+sum=a+b;
+printf("sum of two numbers is %d\n",sum);
+return 0;
+}
