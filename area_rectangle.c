5051d74576b6a7985941e369cf61548d27e99116
Vandana2008 <vandanakethavarapu39@gmail.com>
2025-12-30 22:39:40 +0530

    Add program to find area of rectangle

---
+++ area_rectangle.c
@@ -0,0 +1,11 @@
+//program to find area of recatngle
+#include <stdio.h>
+int main()
+{
+  int length,breadth,area;
+printf("enter length,breadth\n");
+scanf("%d %d ",&length,&breadth);
+area=length*breadth;
+printf("area of rectangle=%d",&area);
+return 0;
+}
